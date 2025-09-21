e11e259528adc1aaa07897ee9714857862e56fad
Shiva-Prasad83 <shivaprasad.ch83@gmail.com>
2025-09-21 13:42:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const container=document.querySelector(".grid-container");
+
+function createGridItems(){
+	for(let i=1;i<=9;i++){
+	 let div=document.createElement("div");
+	  let para=document.createElement("p");
+		para.innerText=i;
+	  div.id=`${i}`;
+		div.className="grid-items";
+		div.appendChild(para);
+	  container.appendChild(div);
+	}
+	
+}
+createGridItems();
+const gridItems=document.querySelectorAll(".grid-items");
+console.log(gridItems);
+const form=document.getElementById("form");
+form.addEventListener("submit",(e)=>{
+	e.preventDefault();
+	let boxNo=document.getElementById("block_id").value;
+	let color=document.getElementById("colour_id").value;
+	let targetBox=document.getElementById(boxNo);
+	removeBgColor();
+	targetBox.style.backgroundColor=color;
+	form.reset();
+});
+
+function removeBgColor(){
+gridItems.forEach((grid)=>{
+	grid.style.backgroundColor="transparent";
+})
+}
